ded241316dd3f4d2eeda04255a421adb94dcb5a6
Br-u-nO <brubauly@gmail.com>
2025-04-12 21:13:10 -0300

    Update index.js

--- Practicas/Practica1/Ejercicio4/index.js
+++ Practicas/Practica1/Ejercicio4/index.js
@@ -1,5 +1,9 @@
 function contarCaracteres(texto){
     console.log(texto.length)
+    console.log(texto.indexOf('ipsum'))
+    let sub= texto.substring(1,4)
+    console.log(sub.toUpperCase())
 }
+let text ='Lorem ipsum dolor sit amet. '
 
 contarCaracteres(text);
