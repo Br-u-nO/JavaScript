b7400fb31fe555dd92f5775fb76bcb081b459cd3
Bruno Bauly <brubauly@gmail.com>
2025-04-27 12:34:54 -0300

    Finalizacion del ejercicio3

--- Practicas/Practica2/Ejercicio3/java.js
+++ Practicas/Practica2/Ejercicio3/java.js
@@ -12,6 +12,7 @@ function hayNumero(valores){
             ok= false;
         return ok;
 }
+
 function  max(values) { 
     let max=-1;
     if((values.length!=0)&&(hayNumero(values))){
@@ -28,40 +29,44 @@ function  min(values) {
     return ("Esta vacio o solo posee elementos no numericos")
 }
 function  avg(values) { 
-    let suma=0,cant=0;
-    if(values.length!=0){
-        values.forEach(element => {
-            if(typeof element=="number")
-                suma+=element;
-                cant+=1;
+    let suma=0;
+    if((values.length!=0)&&(hayNumero(values))){
+        suma=values.reduce(function(ant,act,index){
+            if((typeof act=="number"))
+                return ant+act
+            else{
+                index+=1;
+                return ant;
+            }
         });
-        if(suma!=0)
-            return(suma/cant)
+        return suma/values.length;
     }   
     return ("Esta vacio o solo posee elementos no numericos")
  }
 function  sum(values) { 
-    let suma=0;
-    if(values.length!=0){
-        values.forEach(element => {
-            if(typeof element=="number")
-                suma+=element;
+    if((values.length!=0)&&(hayNumero(values))){
+        return values.reduce(function(ant,act,index){
+            if((typeof act=="number"))
+                return ant+act
+            else{
+                index+=1;
+                return ant;
+            }
         });
-        if(suma!=0)
-            return(suma)
-    }   
+    }
     return ("Esta vacio o solo posee elementos no numericos")
-}
+ }
+
 
 console.log(max(prices));
- console.log(min(prices));
- console.log(avg(prices));
- console.log(sum(prices));
- console.log(max(names));
- console.log(min(names));
- console.log(avg(names));
+console.log(min(prices));
+console.log(avg(prices));
+console.log(sum(prices));
+console.log(max(names));
+console.log(min(names));
+console.log(avg(names));
 console.log(sum(names));
- console.log(max([]));
- console.log(min([]));
- console.log(avg([]));
- console.log(sum([]))
+console.log(max([]));
+console.log(min([]));
+console.log(avg([]));
+console.log(sum([]))
